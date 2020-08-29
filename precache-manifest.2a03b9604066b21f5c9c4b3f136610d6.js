786c56867a8e28004a65d841b2c425c297ef91a1
AraMatevosyan777 <60426627+AraMatevosyan777@users.noreply.github.com>
2020-08-29 17:55:10 +0400

    Updates

---
+++ precache-manifest.2a03b9604066b21f5c9c4b3f136610d6.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c131abcdde83ed243595ea1c298cd481",
+    "url": "/my_portfolio/index.html"
+  },
+  {
+    "revision": "30c8f7cb50daf4aedfad",
+    "url": "/my_portfolio/static/css/2.847e01e0.chunk.css"
+  },
+  {
+    "revision": "562cedc8e22c04eb90dd",
+    "url": "/my_portfolio/static/css/main.ce85b4e2.chunk.css"
+  },
+  {
+    "revision": "30c8f7cb50daf4aedfad",
+    "url": "/my_portfolio/static/js/2.7f3930fc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my_portfolio/static/js/2.7f3930fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "562cedc8e22c04eb90dd",
+    "url": "/my_portfolio/static/js/main.21a8970a.chunk.js"
+  },
+  {
+    "revision": "c73863dbee480bbb5638",
+    "url": "/my_portfolio/static/js/runtime-main.7e1b8294.js"
+  },
+  {
+    "revision": "71d943118e9a1454bb0d0b359ccb7ef3",
+    "url": "/my_portfolio/static/media/Ara Matevosyan.71d94311.pdf"
+  },
+  {
+    "revision": "662a03da901a93233b02818d1ac86d0d",
+    "url": "/my_portfolio/static/media/Mogo.662a03da.png"
+  },
+  {
+    "revision": "170eb974b76cc971535bcc5923377d4b",
+    "url": "/my_portfolio/static/media/Portfolio.170eb974.png"
+  },
+  {
+    "revision": "5b8d31a6eabcd974229f668dee911407",
+    "url": "/my_portfolio/static/media/active-box.5b8d31a6.png"
+  },
+  {
+    "revision": "d527a13c1c8f63b4ef7dc9973ccaf937",
+    "url": "/my_portfolio/static/media/beach-resort.d527a13c.png"
+  },
+  {
+    "revision": "e44d4dd6594e6ae6718a89059f29cdc7",
+    "url": "/my_portfolio/static/media/fb.e44d4dd6.svg"
+  },
+  {
+    "revision": "192a6620a687a9e7e51c0037973a1547",
+    "url": "/my_portfolio/static/media/github.192a6620.svg"
+  },
+  {
+    "revision": "5b1ee4a5748f2339e385f74e9bbc86e3",
+    "url": "/my_portfolio/static/media/homePageImg.5b1ee4a5.jpg"
+  },
+  {
+    "revision": "3c8afea5ee97ad99924808a2ab5fec10",
+    "url": "/my_portfolio/static/media/in.3c8afea5.svg"
+  },
+  {
+    "revision": "ca65ac9149f9366b13e1e9bc455d0e8a",
+    "url": "/my_portfolio/static/media/issues.ca65ac91.png"
+  },
+  {
+    "revision": "f2a5af60418d44e948c0a9c7fa2a9020",
+    "url": "/my_portfolio/static/media/movies.f2a5af60.png"
+  },
+  {
+    "revision": "3800937fed842828b41ab7e5deff290e",
+    "url": "/my_portfolio/static/media/rates.3800937f.png"
+  },
+  {
+    "revision": "2d3e963696769bdcf610a7ef9799c8ef",
+    "url": "/my_portfolio/static/media/soc-net.2d3e9636.png"
+  },
+  {
+    "revision": "1eb9851a850648965e5abce34b28e736",
+    "url": "/my_portfolio/static/media/to-do.1eb9851a.png"
+  },
+  {
+    "revision": "d07b34fb7b5fcb4785256cc76f4c262e",
+    "url": "/my_portfolio/static/media/trello.d07b34fb.png"
+  },
+  {
+    "revision": "61f4933baeaea1e9909111cd2b893178",
+    "url": "/my_portfolio/static/media/weather.61f4933b.png"
+  }
+]);
